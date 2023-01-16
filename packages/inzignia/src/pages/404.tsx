ccf8bb1d3d3f6eab96bc0416788499de57cf6896
Andrew Zigler <andrewzigler@gmail.com>
2023-01-16 00:00:06 -0800

    Test inzignia Netlify rebuild

--- packages/inzignia/src/pages/404.tsx
+++ packages/inzignia/src/pages/404.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import { Layout, Metadata } from "../components/Layout";
-import { NavButton } from "../components/Button";
-import { PageProps } from "gatsby";
+import * as React from "react"
+import { Layout, Metadata } from "../components/Layout"
+import { NavButton } from "../components/Button"
+import { PageProps } from "gatsby"
 
-import * as styles from "../components/Layout/Layout.module.scss";
+import * as styles from "../components/Layout/Layout.module.scss"
 
 const NotFound: React.FC<PageProps> = () => {
   return (
@@ -15,17 +15,17 @@ const NotFound: React.FC<PageProps> = () => {
             The requested resource could not be found. Since this isn't a 3xx
             response, it probably wasn't
             <span style={{ fontStyle: "italic" }}>moo</span>
-            &#8209;ved.
+            &#8209;ved...
           </p>
           <NavButton to={"/"} bgColor={"white"} />
         </div>
       </div>
     </Layout>
-  );
-};
+  )
+}
 
-export default NotFound;
+export default NotFound
 
 export const Head: React.FC<PageProps> = () => {
-  return <Metadata title="404 Not Found" description="URL not found" />;
-};
+  return <Metadata title="404 Not Found" description="URL not found" />
+}
